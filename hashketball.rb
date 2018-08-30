dc4a18f6f985ed6f9dd1f4d216da58251ea1cab7
Camille Rheaume-Tremblay <camillerheaumet@hotmail.com>
2018-08-30 14:04:42 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,7 +183,7 @@ def player_stats(player_name)
   game_hash.each do |team, team_infos|
     game_hash[team][:players].each do |player|
       if player[:player_name] == player_name
-        p player
+        p player.class
       end
     end
   end
